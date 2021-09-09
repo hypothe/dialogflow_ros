73c9ea1a3193eb2b606854c6c375c654aa41f701
Marco Gabriele Fedozzi <73471964+hypothe@users.noreply.github.com>
2021-09-09 14:54:48 +0200

    ensurepip

--- instructions.sh
+++ instructions.sh
@@ -3,7 +3,9 @@ echo "export CLOUDSDK_CORE_DISABLE_PROMPTS=1" >> ${HOME}/.bashrc
 echo "export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/json_folder/gentle-proton-252714-066b7ef02309.json" >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
 
-apt-get install python-pip=20.0.2 python3-pip -y
+python -m ensurepip --default-pip
+python3 -m ensurepip --default-pip
+#apt-get install python-pip=20.0.2 python3-pip -y
 
 pip install --upgrade setuptools
 apt install portaudio19-dev python3-pyaudio -y
