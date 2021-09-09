593e7d72d5b9d3318558a866972e55f62c6ad6a5
cakmakcan <cakmak1213@gmail.com>
2021-09-09 06:11:59 +0200

    mm

---
+++ instructions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+xhost local:root
+
+Desktop/pal_docker_utils-master/scripts : sudo docker container ls
+sudo docker exec -it silly_wilson /bin/bash
+
+export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/json_folder/gentle-proton-252714-066b7ef02309.json
+
+/home/user/ws/src/dialogflow_ros/json_folder
+
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+
+su
+sudo aplay -l
+
+apt-get install --yes sox alsa-utils lame
+l
+
+python instructions2.py
+    
+   dialogflow_client/results
+   
+   
+    pip3 install google-cloud-dialogflow
+    
+    pip install dialogflow==1.1.0
+    
+    apt install python-pyaudio
+    
+    apt update && apt install alsa-base pulseaudio
+    
+    cd dialogflow_ros
+pip install -r requirements.txt
+
+    sudo rosdep init
+    rosdep update
+
+
+ git config --global user.email "cakmak1213@gmail.com"
+ git config --global user.name "cakmakcan"
+
+
+
