1a5d265abfb70dd5f13e3c4513b81476c6ab4cff
AliceNardelli <alice.nardelli98@gmail.com>
2021-09-09 11:16:35 +0200

    HRI modules being connected

--- scripts/mic_client.py
+++ scripts/mic_client.py
@@ -10,20 +10,25 @@ import time
 from std_msgs.msg import String
 from dialogflow_ros import DialogflowClient
 from dialogflow_ros.msg import *
+from dialogflow_ros.msg import GetOrderAction, GetOrderResult
+from std_srvs.srv import Trigger
+import actionlib
+
+openmic_cli = None
 
 
 class GspeechClient(object):
     def __init__(self):
-	#wrong_ans = ["You have missed an item or put the wrong item. Please correct the order!", \
-	#	"I didn't get that. Can you say it again?", \
-#		"I missed what you said. What was that?", \
-#		"Sorry, could you say that again?", \
-#"Sorry, can you say that again?", \
-#"Can you say that again?", \
-#"Sorry, I didn't get that. Can you rephrase?", \
-#"Sorry, what was that?", \
-#"One more time?", \
-	self.repeat_intent = ['EOC']
+        # wrong_ans = ["You have missed an item or put the wrong item. Please correct the order!", \
+        #	"I didn't get that. Can you say it again?", \
+        #		"I missed what you said. What was that?", \
+        #		"Sorry, could you say that again?", \
+        # "Sorry, can you say that again?", \
+        # "Can you say that again?", \
+        # "Sorry, I didn't get that. Can you rephrase?", \
+        # "Sorry, what was that?", \
+        # "One more time?", \
+        self.repeat_intent = ['EOC']
 
         # Audio stream input setup
         FORMAT = pyaudio.paInt16
@@ -81,46 +86,44 @@ class GspeechClient(object):
         multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
         print only the transcription for the top alternative of the top result.
         """
-	for response in responses:
-		
-		# If not a valid response, move on to next potential one
-		if not response.results:
-				continue
-		print("I heard: %s" % response)		
-		# The `results` list is consecutive. For streaming, we only care about
-		# the first result being considered, since once it's `is_final`, it
-		# moves on to considering the next utterance.
-		result = response.results[0]
-		if not result.alternatives:
-			continue
-	
-		
-		# Display the transcription of the top alternative.
-		transcript = result.alternatives[0].transcript
+        for response in responses:
 
-		# Parse the final utterance
-		if result.is_final:
-			rospy.loginfo("Google Speech result: {}".format(result))
-			# Received data is Unicode, convert it to string
-			transcript = transcript.encode('utf-8')
-			# Strip the initial space, if any
-			if transcript.startswith(' '):
-				transcript = transcript[1:]
-		    
+            # If not a valid response, move on to next potential one
+            if not response.results:
+                continue
+            print("I heard: %s" % response)
+            # The `results` list is consecutive. For streaming, we only care about
+            # the first result being considered, since once it's `is_final`, it
+            # moves on to considering the next utterance.
+            result = response.results[0]
+            if not result.alternatives:
+                continue
+
+            # Display the transcription of the top alternative.
+            transcript = result.alternatives[0].transcript
+
+            # Parse the final utterance
+            if result.is_final:
+                rospy.loginfo("Google Speech result: {}".format(result))
+                # Received data is Unicode, convert it to string
+                transcript = transcript.encode('utf-8')
+                # Strip the initial space, if any
+                if transcript.startswith(' '):
+                    transcript = transcript[1:]
+
                     # Exit if needed
-			if transcript.lower() == 'exit':
-				self.shutdown()
-                # Send the rest of the sentence to topic
-			self.text_pub.publish(transcript)
-		
-  		dr = DialogflowRequest(query_text=transcript)
-		resp_tmp = dc.detect_intent_text(dr)
-		resp = resp_tmp.fulfillment_text
-		intent = resp_tmp.intent
-		self.text_pub.publish(resp)	
-		return intent not in self.repeat_intent
+                if transcript.lower() == 'exit':
+                    self.shutdown()
+            # Send the rest of the sentence to topic
+                self.text_pub.publish(transcript)
 
-		
+                dr = DialogflowRequest(query_text=transcript)
+                resp_tmp = dc.detect_intent_text(dr)
+                resp = resp_tmp.fulfillment_text
+                intent = resp_tmp.intent
+                self.text_pub.publish(resp)
+
+            return intent not in self.repeat_intent, resp
 
     def gspeech_client(self):
         """Creates the Google Speech API client, configures it, and sends/gets
@@ -129,24 +132,25 @@ class GspeechClient(object):
         language_code = 'en-US'
         client = speech.SpeechClient()
         config = types.RecognitionConfig(
-			encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-			sample_rate_hertz=44100,
-			language_code=language_code)
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=44100,
+            language_code=language_code)
         streaming_config = types.StreamingRecognitionConfig(
-			config=config,
-			interim_results=True)
-	dc = DialogflowClient()
-	repeat = True
-	while repeat and not rospy.is_shutdown():
-		print("listening for msg")
-  	    # Hack from Google Speech Python docs, very pythonic c:
-		requests = (types.StreamingRecognizeRequest(audio_content=content) for content in self._generator())
-		responses = client.streaming_recognize(streaming_config, requests)
-		print("Message taken")
-		repeat = self._listen_print_loop(responses, dc)
-		#dc = DialogflowClient()
-		#dr = DialogflowRequest(query_text=transcript)
-		#resp1 = dc.detect_intent_text(dr)
+            config=config,
+            interim_results=True)
+        dc = DialogflowClient()
+        repeat = True
+        while repeat and not rospy.is_shutdown():
+            print("listening for msg")
+            responses=openmic_cli()
+            print("Message taken")
+            repeat, text = self._listen_print_loop(responses, dc)
+            #dc = DialogflowClient()
+            #dr = DialogflowRequest(query_text=transcript)
+            #resp1 = dc.detect_intent_text(dr)
+
+        final_order = text 
+        return final_order   
 
     def shutdown(self):
         """Shut down as cleanly as possible"""
@@ -161,13 +165,31 @@ class GspeechClient(object):
         """Entry function to start the client"""
         try:
             rospy.loginfo("Starting Google speech mic client")
-            self.gspeech_client()
+            final_order=self.gspeech_client()
         except KeyboardInterrupt:
             self.shutdown()
+
+        return final_order    
+
+
+def action_clbk(req):
+    g = GspeechClient()
+    final_order=g.start_client()
+    resp=GetOrderResult()
+    resp.final_order=final_order
+
+    _as.set_succeeded(resp)
+
+
 
 
 if __name__ == '__main__':
     rospy.init_node('dialogflow_mic_client')
-    g = GspeechClient()
-    g.start_client()
 
+    _as = actionlib.SimpleActionServer(
+        'requested_by_hri',
+        GetOrderAction,
+        execute_cb=action_clbk,
+        auto_start=False)
+    _as.start()
+    openmic_cli = rospy.ServiceProxy('listen', Trigger)
