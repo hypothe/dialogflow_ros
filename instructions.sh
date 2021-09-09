76ce20d62fcf3d36d34f1efe676b8ade6f124ba3
hypothe <markeagle98@gmail.com>
2021-09-09 15:32:16 +0200

    revert

--- instructions.sh
+++ instructions.sh
@@ -2,16 +2,12 @@
 echo "export CLOUDSDK_CORE_DISABLE_PROMPTS=1" >> ${HOME}/.bashrc
 echo "export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/json_folder/gentle-proton-252714-066b7ef02309.json" >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
+apt-get install python-pip python3-pip -y
 
-python -m ensurepip --default-pip
-python3 -m ensurepip --default-pip
-#apt-get install python-pip=20.0.2 python3-pip -y
-
-#pip install --upgrade setuptools
+pip install --upgrade setuptools
 apt install portaudio19-dev python3-pyaudio -y
 
 pip install --upgrade google-cloud-speech
-
 pip install -r requirements.txt 
 
 pip install dialogflow
