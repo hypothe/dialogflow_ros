bdac0fa4a524c81c7c87e39b78da9713d60daa7e
hypothe <markeagle98@gmail.com>
2021-09-09 15:40:17 +0200

    frozen dep

--- instructions.sh
+++ instructions.sh
@@ -4,15 +4,8 @@ echo "export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/jso
 source ${HOME}/.bashrc
 apt-get install python-pip python3-pip -y
 
-pip install --upgrade setuptools
-apt install portaudio19-dev python3-pyaudio -y
-
-pip install --upgrade google-cloud-speech
 pip install -r requirements.txt 
-
-pip install dialogflow
-python3 -m pip install dialogflow
-
+pip3 install -r requirements3.txt 
 
 apt install python-pyaudio -y
 
