4c492e495271c64eecb18f9c7753976b5d67eaba
hypothe <markeagle98@gmail.com>
2021-09-09 10:33:12 +0200

    typos

--- instructions.sh
+++ instructions.sh
@@ -2,38 +2,41 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/json_folder/gentle-proton-252714-066b7ef02309.json
 
-/home/user/ws/src/dialogflow_ros/json_folder
+pip3 install -r requirements.txt
+
+#sudo curl https://sdk.cloud.google.com | sudo bash https://sdk.cloud.google.com
+#curl -sSL https://sdk.cloud.google.com | bash
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get install apt-transport-https ca-certificates gnupg curl -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update && apt-get install google-cloud-sdk -y
 
 gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
 
-su
-sudo aplay -l
-
 apt-get install --yes sox alsa-utils lame
-l
+
+pip3 install google-cloud-dialogflow-cx
+pip3 install dialogflow==1.1
+pip3 install dialogflow_v2beta1
+
+echo "AAA"
 
 python instructions2.py
-    
-   dialogflow_client/results
+
    
-   
-    pip3 install google-cloud-dialogflow
+pip install google-cloud-dialogflow
     
-    pip install dialogflow==1.1.0
-    
-    apt install python-pyaudio
-    
-    apt update && apt install alsa-base pulseaudio
-    
-    cd dialogflow_ros
-pip install -r requirements.txt
 
-    sudo rosdep init
-    rosdep update
+apt install python-pyaudio -y
 
+apt update && apt install alsa-base pulseaudio -y
 
- git config --global user.email "cakmak1213@gmail.com"
- git config --global user.name "cakmakcan"
+#cd dialogflow_ros
+
+sudo rosdep init
+rosdep update
+
 
 
 
