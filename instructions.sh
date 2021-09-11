86eb44f5dfe3d511ac249e5cb45f22c83a7c91b4
hypothe <markeagle98@gmail.com>
2021-09-11 12:22:52 +0200

    <Add> curl, source, portaudio

--- instructions.sh
+++ instructions.sh
@@ -4,6 +4,13 @@ echo "export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/jso
 source ${HOME}/.bashrc
 apt-get install python-pip python3-pip -y
 
+apt-get install portaudio19-dev
+
+curl https://sdk.cloud.google.com | bash
+source "${HOME}/google-cloud-sdk/path.bash.inc"
+source "${HOME}/google-cloud-sdk/completion.bash.inc"
+
+
 pip install -r requirements.txt 
 pip3 install -r requirements3.txt 
 
@@ -18,4 +25,3 @@ gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
 
 sudo rosdep init
 rosdep update
-
