f88616e8396a89f1a0424e31206280c952790363
hypothe <markeagle98@gmail.com>
2021-09-14 19:30:36 +0200

    <Fix> pyhton pip

--- instructions.sh
+++ instructions.sh
@@ -2,7 +2,6 @@
 echo "export CLOUDSDK_CORE_DISABLE_PROMPTS=1" >> ${HOME}/.bashrc
 echo "export GOOGLE_APPLICATION_CREDENTIALS=/home/user/ws/src/dialogflow_ros/json_folder/gentle-proton-252714-066b7ef02309.json" >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
-apt-get install python-pip python3-pip -y
 
 apt-get install portaudio19-dev -y
 
@@ -10,6 +9,8 @@ curl https://sdk.cloud.google.com | bash
 source "${HOME}/google-cloud-sdk/path.bash.inc"
 source "${HOME}/google-cloud-sdk/completion.bash.inc"
 
+apt-get install python-setuptools  python3-pip -y
+easy_install pip
 python -m pip install -r requirements.txt 
 python3 -m pip install -r requirements3.txt 
 
